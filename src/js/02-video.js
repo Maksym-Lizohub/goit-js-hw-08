c272e79f1928007349fe26a35054a4b1a757523e
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-07-03 21:55:06 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const KEY_LOCAL_STORAGE = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+/* получить текущее время */
+
+player.on('timeupdate', function (data) {
+  console.log('timeupdate :>> ', data);
+  // data is an object containing properties specific to that event
+});
+
+/* установить начальное время */
+
+player
+  .setCurrentTime(KEY_LOCAL_STORAGE)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
